42c64c4a4dcac818c9b283c9920be410a89d1e60
eliastam <elias.tamraz@mail.mcgill.ca>
2020-10-25 22:57:34 -0400

    TestCategoryDone

--- NUnitTestProject1/ApiHelper.cs
+++ NUnitTestProject1/ApiHelper.cs
@@ -13,13 +13,24 @@ namespace NUnitTestProject1
 {
     public static class ApiHelper
     {
+
         public static HttpClient ApiClient { get; set; } = new HttpClient();
+        public async static Task<int> GetAnId()
+        {
+            var id = 0;
+            var list = await GetAllCategories();
+            if (list.Any())
+            {
+                id = list.First().id;
+            }
+            return id;
+        }
         public async static Task<IEnumerable<Object>> GetAllTodos()
         {
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-                HttpResponseMessage response =  client.GetAsync("http://localhost:4567/todos").Result;
+                HttpResponseMessage response = client.GetAsync("http://localhost:4567/todos").Result;
 
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
@@ -27,7 +38,8 @@ namespace NUnitTestProject1
 
 
                     var studentLst = xdoc.Descendants("todo").Select(d =>
-                      new Todo {
+                      new Todo
+                      {
                           id = Int32.Parse(d.Element("id").Value),
                           doneStatus = bool.Parse(d.Element("doneStatus").Value),
                           title = d.Element("title").Value,
@@ -43,7 +55,7 @@ namespace NUnitTestProject1
             }
 
         }
-   
+
         public async static Task CreateTodo(string doneStatus, string description, string title)
         {
             var httpClient = new HttpClient();
@@ -102,11 +114,11 @@ namespace NUnitTestProject1
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-                 var response =await client.PutAsync("http://localhost:4567/categories", stringContent);
+                var response = await client.PutAsync("http://localhost:4567/categories", stringContent);
                 return response;
             }
 
-        } 
+        }
         public async static Task<HttpResponseMessage> createCategorySuccessfully()
         {
             var httpClient = new HttpClient();
@@ -127,9 +139,19 @@ namespace NUnitTestProject1
             var respone = await httpClient.PostAsync("http://localhost:4567/categories", stringContent);
             return respone;
         }
+        public async static Task<HttpResponseMessage> createCategorySuccessfullyWithoutDescription()
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<category><description/><title>PostValue</title></category>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync("http://localhost:4567/categories", stringContent);
+            return respone;
+        }
         public async static Task<HttpResponseMessage> DeleteCategory()
         {
-        
+
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
@@ -153,6 +175,17 @@ namespace NUnitTestProject1
 
         #region /categroy/id
         public async static Task<HttpResponseMessage> GetCategoryById(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                HttpResponseMessage response = client.GetAsync($"http://localhost:4567/categories/{id}").Result;
+
+                return response;
+            }
+
+        }
+        public async static Task<HttpResponseMessage> GetCategoryByIdString(string id)
         {
             using (var client = new HttpClient())
             {
@@ -232,6 +265,200 @@ namespace NUnitTestProject1
                 return response;
             }
 
+        }
+        #endregion
+
+        #region /categroy/id/projects
+        public async static Task<HttpResponseMessage> CreateCategoryProjectRelationship(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<projects><id>1</id></projects>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync($"http://localhost:4567/categories/{categoryId}/projects", stringContent);
+            return respone;
+        }
+
+        public async static Task<HttpResponseMessage> DeleteCategoryIdProjects(int categoryId)
+        {
+            var httpClient = new HttpClient();
+
+
+            var respone = await httpClient.DeleteAsync($"http://localhost:4567/categories/{categoryId}/projects");
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> GetCategoryIdProject(int categoryId)
+        {
+            var httpClient = new HttpClient();
+
+            var respone = await httpClient.GetAsync($"http://localhost:4567/categories/{categoryId}/projects");
+            return respone;
+        }
+
+
+        public async static Task<HttpResponseMessage> GetCategoryIdStringProject(string categoryId)
+        {
+            var httpClient = new HttpClient();
+
+            var respone = await httpClient.GetAsync($"http://localhost:4567/categories/{categoryId}/projects");
+            return respone;
+        }
+        #endregion
+
+        #region /categroy/id/projects/id
+
+        public async static Task<HttpResponseMessage> PostCategoryIdProjectId(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<projects><id>1</id></projects>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync($"http://localhost:4567/categories/{categoryId}/projects/{projectId}", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PutCategoryIdProjectId(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<projects><id>1</id></projects>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PutAsync($"http://localhost:4567/categories/{categoryId}/projects/{projectId}", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PatchCategoryIdProjectId(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<projects><id>1</id></projects>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PatchAsync($"http://localhost:4567/categories/{categoryId}/projects/{projectId}", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> DeleteCategoryIdProjectsId(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+
+
+            var respone = await httpClient.DeleteAsync($"http://localhost:4567/categories/{categoryId}/projects/{projectId}");
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> GetCategoryIdProjectId(int categoryId, int projectId)
+        {
+            var httpClient = new HttpClient();
+
+            var respone = await httpClient.GetAsync($"http://localhost:4567/categories/{categoryId}/projects/{projectId}");
+            return respone;
+        }
+
+
+        #endregion
+
+        #region/categories/:id/todos
+        public async static Task<HttpResponseMessage> GetCategoryIdTodos(int categoryId)
+        {
+            var httpClient = new HttpClient();
+           
+            var respone = await httpClient.GetAsync($"http://localhost:4567/categories/{categoryId}/todos");
+            return respone;
+        }
+
+        public async static Task<HttpResponseMessage> PostCategoryIdTodos(int categoryId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync($"http://localhost:4567/categories/{categoryId}/todos", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PostCategoryIdTodoswithError(int categoryId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync($"http://localhost:4567/categories/{categoryId}/todos", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PutCategoryIdTodos(int categoryId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PutAsync($"http://localhost:4567/categories/{categoryId}/todos", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PatchCategoryIdTodos(int categoryId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PatchAsync($"http://localhost:4567/categories/{categoryId}/todos", stringContent);
+            return respone;
+        }
+
+        public async static Task<HttpResponseMessage> DeleteCategoryIdTodos(int categoryId)
+        {
+            var httpClient = new HttpClient();
+
+            var respone = await httpClient.DeleteAsync($"http://localhost:4567/categories/{categoryId}/todos");
+            return respone;
+        }
+
+
+
+        #endregion
+
+        #region/categories/:id/todos
+        public async static Task<HttpResponseMessage> GetCategoryIdTodosId(int categoryId, int todoId)
+        {
+            var httpClient = new HttpClient();
+
+            var respone = await httpClient.GetAsync($"http://localhost:4567/categories/{categoryId}/todos/{todoId}");
+            return respone;
+        }
+
+
+        public async static Task<HttpResponseMessage> PostCategoryIdTodosId(int categoryId, int todoId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PostAsync($"http://localhost:4567/categories/{categoryId}/todos/{todoId}", stringContent);
+            return respone;
+        }
+
+        public async static Task<HttpResponseMessage> PutCategoryIdTodosId(int categoryId, int todoId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PutAsync($"http://localhost:4567/categories/{categoryId}/todos/{todoId}", stringContent);
+            return respone;
+        }
+        public async static Task<HttpResponseMessage> PatchCategoryIdTodosId(int categoryId, int todoId)
+        {
+            var httpClient = new HttpClient();
+            var someXmlString = $"<todo><doneStatus>false</doneStatus><description>ddd</description><title>title</title></todo>";
+
+
+            var stringContent = new StringContent(someXmlString, Encoding.UTF8, "application/xml");
+            var respone = await httpClient.PatchAsync($"http://localhost:4567/categories/{categoryId}/todos/{todoId}", stringContent);
+            return respone;
         }
         #endregion
     }
--- NUnitTestProject1/TestCategory.cs
+++ NUnitTestProject1/TestCategory.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -63,7 +64,17 @@ namespace NUnitTestProject1
             Assert.AreEqual(CountBeforeAddition , CountAfterAddition);
             Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
+        
+        [Test]
+        public async Task TestSucessfulPostCategorieswithoutDescription()
+        {
 
+            var response = await ApiHelper.createCategorySuccessfullyWithoutDescription();
+
+          
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+        
         [Test]
         public async Task TestDeleteCategory()
         {
@@ -72,6 +83,7 @@ namespace NUnitTestProject1
 
             Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.Result.StatusCode);
         }
+       
         [Test]
         public async Task TestpatchCategory()
         {
@@ -81,7 +93,6 @@ namespace NUnitTestProject1
             Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.Result.StatusCode);
         }
         #endregion
-
         #region /categories/id
         [Test]
         public async Task TestSuccessfulGetCategorybyId()
@@ -125,6 +136,14 @@ namespace NUnitTestProject1
 
 
             Assert.AreEqual(0, categories.Count());
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        // Issue 1 : when filter by id contains character it gives not found instead of an error
+        public async Task TestUnsuccessfulGetCategorybyIdContainsCharacter()
+        {
+            var response = await ApiHelper.GetCategoryByIdString("invalildID$$soInvalid2020");
+
+          
             Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
         [Test]
@@ -235,6 +254,196 @@ namespace NUnitTestProject1
         }
 
         #endregion
+        #region /categories/id/projects
+        [Test]
+        public async Task TestDeleteCategoryIdProject()
+        {
+            var id = await ApiHelper.GetAnId();
 
+            var response = await ApiHelper.DeleteCategoryIdProjects(id);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+
+        [Test]
+        public async Task TestGetCategoryIdProject()
+        {
+            var id = await ApiHelper.GetAnId();
+
+            var response = await ApiHelper.GetCategoryIdProject(id);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        // Issue 2: this should return not found  
+        [Test]
+        public async Task TestGetCategoryIdProjectUnExpected()
+        {
+            var response = await ApiHelper.GetCategoryIdProject(0);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        // Issue 3: Id must be a number characters should not be accepted
+        [Test]
+        public async Task TestGetCategoryIdProjectUnExpected2()
+        {
+            var response = await ApiHelper.GetCategoryIdStringProject("StringID%%");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+        #endregion
+        #region /categories/id/projects
+
+        // issue 4: should be method not allowd ! 
+        [Test]
+        public async Task TestPostCategoryIdProjectId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PostCategoryIdProjectId(id, 909);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        // issue 5: should be method not allowd ! 
+        [Test]
+        public async Task TestGetCategoryIdProjectId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.GetCategoryIdProjectId(id,909);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Test]
+        public async Task TestPutCategoryIdProjectId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PutCategoryIdProjectId(id, 909);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+  
+        [Test]
+        public async Task TestPatchCategoryIdProjectId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PatchCategoryIdProjectId(id, 909);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [Test]
+        public async Task TestDeleteCategoryIdProjectId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.DeleteCategoryIdProjectsId(id, 909);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        #endregion
+        #region/categories/:id/todos
+
+        [Test]
+        public async Task TestGetCategoryIdTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.GetCategoryIdTodos(id);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Test]
+        public async Task TestPostCategoryIdTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PostCategoryIdTodos(id);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+        [Test]
+        public async Task TestPostCategoryIdnotExistedTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PostCategoryIdTodos(0);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Test]
+        public async Task TestPostCategoryIdTodoswithError()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PostCategoryIdTodoswithError(id);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task TestPutCategoryIdTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PutCategoryIdTodos(id);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        [Test]
+        public async Task TestPatchCategoryIdTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PatchCategoryIdTodos(id);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+        [Test]
+        public async Task TestDeleteCategoryIdTodos()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.DeleteCategoryIdTodos(id);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+
+        #endregion
+        #region/categories/:id/todos
+        [Test]
+        // issue 6: should be method not allowd ! 
+        public async Task TestGetCategoryIdTodosId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.GetCategoryIdTodosId(id, 9);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        // issue 7: should be method not allowd ! 
+        [Test]
+        public async Task TestPostCategoryIdTodosId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PostCategoryIdTodosId(id, 9);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Test]
+
+        public async Task TestPutCategoryIdTodosId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PutCategoryIdTodosId(id, 9);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+        [Test]
+        public async Task TestPatchCategoryIdTodosId()
+        {
+            var id = await ApiHelper.GetAnId();
+            var response = await ApiHelper.PatchCategoryIdTodosId(id, 9);
+
+            Assert.AreEqual(HttpStatusCode.MethodNotAllowed, response.StatusCode);
+        }
+
+        #endregion
     }
 }
